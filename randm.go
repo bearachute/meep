d9471c374940038d9f6691cf6eb4759baaa7721a
bearachute <sjourand@gmail.com>
2023-03-19 11:16:54 -0500

    all some new files chilllllld

---
+++ randm.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+"fmt"
+"math/rand"
+)
+
+func main() {
+fmt.Println("my fav number is", rand.Intn(10))
+}
---
+++ typeinf.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+v := 42 // type inference
+b := 3.12 // type i
+q := bool
+d := 0.865 + 0.51
+fmt.Printf("v is %T\n", v)
+fmt.Printf("b is %T\n", b)
+fmt.Printf("q is %T\n", q)
+fmt.Printf("d is %T\n", d)
+}
